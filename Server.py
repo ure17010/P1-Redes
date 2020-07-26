a5597c36026e99f55c3e946cccc442ef93166311
ure17010 <68790382+ure17010@users.noreply.github.com>
2020-07-26 17:44:23 -0600

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,51 +1,10 @@
 """
-import socket
-from _thread import *
-import sys
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #conexion socket
-
-#server y puerto definido
-server = 'localhost'
-port = 5555
-
-server_ip = socket.gethostbyname(server)
-
-#unir server con el puerto
-try:
-    s.bind((server, port))
-
-#mensaje de fallo
-except socket.error as e:
-    print(str(e))
-
-#busca conexiones al server
-s.listen(3) #tres jugadores maximo
-print("Waiting for a connection")
-
-#tras tener ya una conexion de 3
-
-IdActual = "0"
-
-#espacio para funcion con ID (para cada usuario) y !!!NOT SURE!!!!
-
-
-
-#para que mantenga reciviendo las diferentes conexiones
-while True:
-    conn, addr = s.accept()
-    print("Connected to: ", addr)
-
-    start_new_thread(threaded_client, (conn,))
-"""
-
-"""
 Universidad del Valle de Guatemala
 Redes 
 Catedrático: Vinicio 
 Pablo Viana - Estuardo Ureta - Oliver Mazariegos 
 
-Proyecto 1: juego de cartas ¿ya no es sequence?
+Proyecto 1: juego de cartas: OLD MAID
 """
 
 import socket, threading
