d3e109788715cf4c1289dafa4f2fce10444e8449
Oliversinn <https://Oliversinn@github.com>
2020-07-29 22:16:35 -0600

    server y cliente conectandose pero con vergueos

---
+++ client-test.py
@@ -0,0 +1,142 @@
+import socket
+import select
+import errno
+import sys
+import pickle
+
+HEADER_LENGTH = 10 
+IP = "127.0.0.1"
+PORT = 5555
+
+def receive_message(client_socket):
+    try:
+        message_header = client_socket.recv(HEADER_LENGTH)
+        if not len(message_header):
+            return False
+        
+        print(f"message_header: {message_header}")
+        message_length  = int(message_header.decode('utf-8').strip())
+        data = pickle.loads(client_socket.recv(message_length))
+        print(f"data: {data}")
+        msg = {"header": message_header, "data": data}
+        print(f"msg: {msg}")
+        return {"header": message_header, "data": data}
+
+
+    except Exception as e:
+        print(e)
+        return False
+
+
+def signinok(roomID):
+    dprotocol = {
+        "type":"signinok",
+        "roomID": roomID
+    }
+    # serializing dprotocol
+    msg = pickle.dumps(dprotocol)
+    # adding header to msg
+    msg = bytes(f"{len(msg):<{HEADER_LENGTH}}", "utf-8") + msg
+    return msg
+
+def sendmessage(message):
+    dprotocol = {
+        "type":"sendmessage",
+        "message": message
+    }
+    # serializing dprotocol
+    msg = pickle.dumps(dprotocol)
+    # adding header to msg
+    msg = bytes(f"{len(msg):<{HEADER_LENGTH}}", "utf-8") + msg
+    return msg
+
+
+# make conncection
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((IP,PORT))
+client_socket.setblocking(False)
+print(f"Connected to server in {IP}:{PORT}")
+signedin = False
+
+my_username = input("Username: ")
+dprotocol = {
+    'type': 'signin',
+    'username': my_username
+}
+# serializing dprotocol
+msg = pickle.dumps(dprotocol)
+# adding header to msg
+msg = bytes(f"{len(msg):<{HEADER_LENGTH}}", "utf-8") + msg
+client_socket.send(msg)
+
+while not signedin:
+    try:
+        while True:
+            #wait for useraccepted
+            message = receive_message(client_socket)
+            if message:
+                if message['data']['username'] == my_username:
+                    # send signinok
+                    print(f"Singned in server @{IP}:{PORT} as {my_username}")
+                    msg = signinok(message['data']['roomID'])
+                    client_socket.send(msg)
+                    signedin = True
+                    break
+                else:
+                    print(f"Server thought you were {message['data']['username']}")
+                    print("Disconnecting...")
+                    sys.exit()
+
+    except IOError as e:
+        # errores de lectura
+        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+            print('Reading error',str(e))
+            sys.exit()
+        continue
+
+    except Exception as e:
+        print('General error', str(e))
+        sys.exit()
+
+
+
+
+while True:
+    message = input(f"{my_username} > ")
+
+
+    if message:
+        msg = sendmessage(message)
+        client_socket.send(msg)
+
+    try:
+        while True:
+            # receive things
+            # username_header = client_socket.recv(HEADER_LENGTH)
+            # if not len(username_header):
+            #     print("Connection closed by the server")
+            #     sys.exit()
+            msg = receive_message(client_socket)
+
+            if not len(msg):
+                print("Connection closed by the server")
+                sys.exit()
+
+            #msg = receive_message(client_socket)
+            print(msg)
+            username = msg['data']['username']
+            message = msg['data']['message']
+
+            print(f"{username} > {message}")
+
+    except IOError as e:
+        # errores de lectura
+        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+            print('Reading error',str(e))
+            sys.exit()
+        continue
+
+
+    # except Exception as e:
+    #     print('General error', str(e))
+    #     sys.exit()
---
+++ server-test.py
@@ -0,0 +1,116 @@
+import socket
+import select
+import pickle
+
+HEADER_LENGTH = 10 
+IP = "127.0.0.1"
+PORT = 5555
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#para permitir reconectar
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_socket.bind((IP,PORT))
+server_socket.listen()
+print(f"Server listening in {IP}:{PORT}")
+
+sockets_list = [server_socket]
+
+clients = {}
+
+def receive_message(client_socket):
+    try:
+        message_header = client_socket.recv(HEADER_LENGTH)
+        if not len(message_header):
+            return False
+        
+        message_length  = int(message_header.decode('utf-8').strip())
+        data = pickle.loads(client_socket.recv(message_length))
+        return {"header": message_header, "data": data}
+
+
+    except:
+        return False
+
+def useraccepted(username):
+    dprotocol = {
+        'type': 'useraccepted',
+        'username': username,
+        'roomID': 1
+    }
+    # serializing dprotocol
+    msg = pickle.dumps(dprotocol)
+    print(msg)
+    # adding header to msg
+    msg = bytes(f"{len(msg):<{HEADER_LENGTH}}", "utf-8") + msg
+    print(msg)
+    return msg
+
+def broadcast(username,message):
+    dprotocol = {
+        'type': 'message',
+        'username': username,
+        'message': message
+    }
+    # serializing dprotocol
+    msg = pickle.dumps(dprotocol)
+    print(msg)
+    # adding header to msg
+    msg = bytes(f"{len(msg):<{HEADER_LENGTH}}", "utf-8") + msg
+    print(msg)
+    return msg
+
+
+while True:
+    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
+
+    for notified_socket in read_sockets:
+
+        # cuando alguien se acaba de conectar al server
+        if notified_socket == server_socket:
+            client_socket, client_address = server_socket.accept()
+            user = receive_message(client_socket)
+            if user is False:
+                continue
+            if user['data']['type'] == 'signin':
+                msg = useraccepted(user['data']['username'])
+                # Sending useraccepted
+                client_socket.send(msg)
+
+                # Wait for singinok
+                signinok = False
+                while not signinok:
+                    message = receive_message(client_socket)
+                    if message is False:
+                        continue
+                    if message['data']['type'] == "signinok":
+                        signinok = True
+
+                sockets_list.append(client_socket)
+                clients[client_socket] = user
+                print(f"Accepted new connection from {client_address[0]}:{client_address[1]} username: {user['data']['username']}")
+
+        else:
+            message = receive_message(notified_socket)
+            print(message)
+
+            if message is False:
+                print(f"Closed connection from {clients[notified_socket]['data']['username']}")
+                sockets_list.remove(notified_socket)
+                del clients[notified_socket]
+                continue
+            user = clients[notified_socket]
+            
+            if message['data']['type'] == 'sendmessage':
+                print(f"Received message from {user['data']['username']}: {message['data']['message']}")
+                msg = broadcast(user['data']['username'],message['data']['message'])
+                for client_socket in clients:
+                    # send message to every client except the sender
+                    if client_socket != notified_socket:
+                        client_socket.send(msg)
+            else:
+                print(f"El tipo de mensaje es: {message['data']['type']}")
+
+        for notified_socket in exception_sockets:
+            sockets_list.remove(notified_socket)
+            del clients[notified_socket]
